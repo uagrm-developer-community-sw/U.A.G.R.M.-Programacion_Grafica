4c324fc3f2845d95d90973fbb50743a07ef8c811
jhasmany-fernandez <fernandez.jhasmany@ficct.uagrm.edu.bo>
2023-04-08 08:16:16 -0400

    TAREA 2 - Crear una casa y auto en 3D. terminado

---
+++ Tareas/2. Tarea II/Casa_Auto_3D/PGrafica/Casa.cs
@@ -0,0 +1,205 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+namespace PGrafica
+{
+    class Casa
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float altura;
+        public float profundidad;
+        public float anchura;
+        public Casa(float x, float y, float z, float anchura, float altura, float profundidad) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.anchura = anchura;
+            this.altura = altura;
+            this.profundidad = profundidad;
+        }
+        
+
+        public  void Dibujar() {
+            DibujarPared();
+            DibujarTecho();
+            
+        }
+
+
+        private void DibujarTecho()
+        {
+            PTfrontal();
+            PTtrasera();
+            Tderecho();
+            Tizquierdo();
+        }
+
+        private void DibujarPared()
+        {
+            Pderecha();
+            Pizquierda();
+            Pabajo();
+            Parriba();
+            Pfrontal();
+            Ptrasera();
+            Puerta();
+        }
+
+        private void Pfrontal()
+        {
+            float[] _vertices = {
+                x-(anchura/2), y - ((0.65f*altura)/2), z-(profundidad/2), 1.0f, 0.5f, 0.0f,
+                x-(anchura/2), y + ((0.65f*altura)/2), z-(profundidad/2), 1.0f, 0.5f, 0.0f,
+                x+(anchura/2), y - ((0.65f*altura)/2), z-(profundidad/2), 1.0f, 0.5f, 0.0f,
+                x-(anchura/2), y + ((0.65f*altura)/2), z-(profundidad/2), 1.0f, 0.5f, 0.0f,
+                x+(anchura/2), y - ((0.65f*altura)/2), z-(profundidad/2), 1.0f, 0.5f, 0.0f,
+                x+(anchura/2), y + ((0.65f*altura)/2), z-(profundidad/2), 1.0f, 0.5f, 0.0f,
+            };
+
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+        }
+
+        private void Ptrasera()
+        {
+            float[] _vertices = {
+                x+(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 1.0f, 0.5f, 0.0f,
+                x+(anchura/2), y-((0.65f*altura)/2), z+(profundidad/2), 1.0f, 0.5f, 0.0f,
+                x-(anchura/2), y-((0.65f*altura)/2), z+(profundidad/2), 1.0f, 0.5f, 0.0f,
+                x+(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 1.0f, 0.5f, 0.0f,
+                x-(anchura/2), y-((0.65f*altura)/2), z+(profundidad/2), 1.0f, 0.5f, 0.0f,
+                x-(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 1.0f, 0.5f, 0.0f,
+            };
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+
+        }
+
+        private void Pderecha()
+        {
+            float[] _vertices = {
+                x+(anchura/2), y-((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x+(anchura/2), y-((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x+(anchura/2), y+((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x+(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x+(anchura/2), y-((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x+(anchura/2), y+((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+            };
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+
+        }
+
+        private void Pizquierda()
+        {
+            float[] _vertices = {
+                x-(anchura/2), y-((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y-((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y-((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y+((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+
+            };
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+        }
+
+        private void Parriba()
+        {
+            float[] _vertices = {
+                x+(anchura/2), y+((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y+((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x+(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x+(anchura/2), y+((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+
+            };
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+        }
+        private void Pabajo()
+        {
+            float[] _vertices = {
+                x-(anchura/2), y-((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x+(anchura/2), y-((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y-((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x+(anchura/2), y-((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y-((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x+(anchura/2), y-((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+            };
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+
+        }
+
+        private void PTfrontal(){
+            float[] _vertices = {
+                x+(anchura/2), y+((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y+((0.65f*altura)/2), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x, y+((0.35f*altura)+((0.65f*altura)/2)), z-(profundidad/2), 1.0f, 1.0f, 0.0f,
+            };
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+        }
+        private void PTtrasera()
+        {
+            float[] _vertices = {
+                x+(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x-(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+                x, y+((0.35f*altura)+((0.65f*altura)/2)), z+(profundidad/2), 1.0f, 1.0f, 0.0f,
+            };
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+        }
+
+        private void Tderecho()
+        {
+            float[] _vertices = {
+                x+(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x+(anchura/2), y+((0.65f*altura)/2), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x, y+((0.35f*altura)+((0.65f*altura)/2)), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x, y+((0.35f*altura)+((0.65f*altura)/2)), z+(profundidad/2),0.1f, 0.0f, 0.0f,
+                x+(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x, y+((0.35f*altura)+((0.65f*altura)/2)), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+            };
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+
+        }
+
+        private void Tizquierdo()
+        {
+            float[] _vertices = {
+                x-(anchura/2), y+((0.65f*altura)/2), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x-(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x, y+((0.35f*altura)+((0.65f*altura)/2)), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x, y+((0.35f*altura)+((0.65f*altura)/2)), z+(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x-(anchura/2), y+((0.65f*altura)/2), z+(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x, y+((0.35f*altura)+((0.65f*altura)/2)), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+            };
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+             
+        }
+
+        private void Puerta()
+        {
+            float[] _vertices = {
+                x+((anchura/2)*0.2f), y-((0.65f*altura)/2), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x+((anchura/2)*0.2f), y-(((0.65f*altura)/2)*0.3f), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x-((anchura/2)*0.2f), y-((0.65f*altura)/2), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+
+                x-((anchura/2)*0.2f), y-(((0.65f*altura)/2)*0.3f), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x-((anchura/2)*0.2f), y-((0.65f*altura)/2), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+                x+((anchura/2)*0.2f), y-(((0.65f*altura)/2)*0.3f), z-(profundidad/2), 0.1f, 0.0f, 0.0f,
+
+            };
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+        }
+    }
+}
---
+++ Tareas/2. Tarea II/Casa_Auto_3D/PGrafica/Game.cs
@@ -0,0 +1,87 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
+using PGrafica.Common;
+using OpenTK.Mathematics;
+using System;
+namespace PGrafica
+{
+    class Game:GameWindow
+    {
+        private int _vertexBufferObject;
+        private int _vertexArrayObject;
+        public Casa casa,casa2;
+        private Shader _shader;
+        private Matrix4 _view;
+        private Matrix4 _projection;
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings){}
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+        protected override void OnLoad()
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+
+            casa = new Casa(0.0f,0.0f,0.0f,0.5f,0.6f,0.6f);
+            casa2 = new Casa(-0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.6f);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+           
+
+            _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+            _shader.Use();
+
+
+            _view = Matrix4.CreateTranslation(0.0f, 0.0f, -3.0f);
+            //_projection = Matrix4.CreateOrthographicOffCenter(-1f, 1f, -1f, 1f, 0.1f, 100.0f);
+            _projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45f), Size.X / (float)Size.Y, 0.1f, 100.0f);
+            base.OnLoad();
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.BindVertexArray(_vertexArrayObject);
+            var model = Matrix4.Identity * Matrix4.CreateRotationY((float)MathHelper.DegreesToRadians(65.0f));
+
+            _shader.Use();
+            _shader.SetMatrix4("model", model);
+            _shader.SetMatrix4("view", _view);
+            _shader.SetMatrix4("projection", _projection);
+            //casa.Dibujar();
+            casa2.Dibujar();
+            SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, Size.X, Size.Y);
+            base.OnResize(e);
+        }
+
+        protected override void OnUnload()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(_vertexBufferObject);
+
+            GL.DeleteProgram(_shader.Handle);
+
+            
+            base.OnUnload();
+        }
+    }
+}
