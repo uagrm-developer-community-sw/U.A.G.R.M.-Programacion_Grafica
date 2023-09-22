87f4c275d8d324213fc6a80a0447647983893c2e
fernandez.jhasmany@ficct.uagrm.edu.bo <jhasmany-fernandez>
2023-09-22 07:21:58 -0400

    update folder

---
+++ Tareas/Pared_3D/Pared_3D/Pared_3D.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pared_3D
+{
+    internal class Pared_3D
+    {
+        GameWindow window;
+        public Pared_3D(GameWindow window)
+        {
+            this.window = window;
+            Start();
+        }
+
+        void Start()
+        {
+            window.Load += Loaded;
+            window.Resize += resise;
+            window.RenderFrame += renderF;
+            window.Run(1.0/60.0);
+        }
+
+        void Loaded(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
