af379510c3dcc7f5fcf0775fa404a0375341c63d
jhasmany jhunnior fernandez ortega <jhasmany.fernandez.dev@gmail.com>
2024-06-20 14:07:20 -0400

    MenuInicio-Roblox

--- 2 - Roblox/Game-Roblox-Vscode/Vsc-Roblox/src/StarterGui/MenuInicio/Frame/Ajustes/Frame/TxtLblSombra/TxtBtnSombraSi/LocalScript.client.lua
+++ 2 - Roblox/Game-Roblox-Vscode/Vsc-Roblox/src/StarterGui/MenuInicio/Frame/Ajustes/Frame/TxtLblSombra/TxtBtnSombraSi/LocalScript.client.lua
@@ -1,3 +1,3 @@
-script.Parent.MouseButton1Click:Connect(function()
-	game.Lighting.GlobalShadows = true
-end)
+script.Parent.MouseButton1Click:Connect(function() -- Conecte una función a la señal MouseButton1Click del objeto de la instancia de script (MouseButton1Click es la señal que se activa cuando se hace clic en el objeto de la instancia de script)
+	game.Lighting.GlobalShadows = true -- Establezca la propiedad GlobalShadows del objeto de la instancia de juego en verdadero (GlobalShadows es la propiedad que controla si las sombras globales están habilitadas)
+end) -- Termine la función
