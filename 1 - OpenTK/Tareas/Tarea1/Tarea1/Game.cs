a398038bcf914cdb86d77f4cb4a6af76f73cf8fb
jhasmany jhunnior fernandez ortega <jhasmany.fernandez.dev@gmail.com>
2024-07-20 03:49:15 -0400

    tarea realizada
    dfasdfa

---
+++ 1 - OpenTK/Tareas/Tarea1/Tarea1/Game.cs
@@ -0,0 +1,33 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea1
+{
+    internal class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { } // constructor
+
+        protected override void OnUpdateFrame(FrameEventArgs e) // override de la clase GameWindow para actualizar el marco
+        {
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e) // override de la clase GameWindow para renderizar el marco
+        {
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnLoad(EventArgs e) // override de la clase GameWindow para cargar el juego
+        {
+            base.OnLoad(e);
+        }
+
+        protected override void OnUnload(EventArgs e) // override de la clase GameWindow para descargar el juego
+        {
+            base.OnUnload(e);
+        }
+    }
+}
---
+++ 1 - OpenTK/Tareas/Tarea1/Tarea1/Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game(800, 600, "OpenTk")) // instancia de la clase Game
+            {
+                game.Run(60.0); // corre el juego a 60 fps
+            }
+        }
+    }
+}
