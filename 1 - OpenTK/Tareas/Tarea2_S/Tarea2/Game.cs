0b7c5f0dd1b8d2b3fd0307dad8a21c7946ce824d
jhasmany jhunnior fernandez ortega <jhasmany.fernandez.dev@gmail.com>
2024-08-17 19:10:28 -0400

    Tarea2 10

---
+++ 1 - OpenTK/Tareas/Tarea2_S/Tarea2/Game.cs
@@ -0,0 +1,30 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea2
+{
+    class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(width, height, OpenTK.Graphics.GraphicsMode.Default, title) { } // Constructor de la clase
+        // ------------------------------------------------------
+        // METODO QUE SE LLAMA AL CARGAR EL PROGRAMA POR PRIMERA VEZ
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+        // ------------------------------------------------------
+        // METODO QUE SE LLAMA AL ACTUALIZAR EL PROGRAMA
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+        }
+        // ------------------------------------------------------
+        // METODO QUE SE LLAMA AL DIBUJAR EL PROGRAMA
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+        }
+    }
+}
---
+++ 1 - OpenTK/Tareas/Tarea2_S/Tarea2/Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea2
+{
+    internal class Program
+    {
+        static void Main(string[] args) // Punto de entrada del programa
+        {
+            using (Game game = new Game(800, 600, "Letra T")) // Ventana de 800x600 con título "Letra T"
+            {
+                game.Run(60.0); // Correr a 60 FPS
+            }
+        }
+    }
+}
