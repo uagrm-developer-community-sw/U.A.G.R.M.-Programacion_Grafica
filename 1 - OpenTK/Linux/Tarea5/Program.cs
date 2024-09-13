993a66de822fe62217869620a71efc54b2727029
jhasmany jhunnior fernandez ortega <fernandez.jhasmany@ficct.uagrm.edu.bo>
2024-09-13 17:16:34 -0400

    tarea5 linux

---
+++ 1 - OpenTK/Linux/Tarea5/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Tarea5 // Cambiar por el nombre de la carpeta que contiene el proyecto
+{
+    public class Program // Clase principal de la aplicación (entry point)
+    {
+        static void Main(string[] args) // Método principal de la aplicación (entry point)
+        {
+            using (Game game = new Game(800, 600, "Letra T")) // 800x600 pixels de resolución
+            {
+                game.Run(60.0); // 60 fotogramas por segundo (FPS)
+
+            }
+        }
+    }
+}
