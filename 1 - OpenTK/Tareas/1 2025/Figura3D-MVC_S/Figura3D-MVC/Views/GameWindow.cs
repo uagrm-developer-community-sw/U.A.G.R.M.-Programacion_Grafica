967f1d131ee3c10d7e076fbfd20b149b90e2b970
jhasmany-fernandez <jhasmany.fernandez.dev@gmail.com>
2025-02-19 21:47:25 -0400

    cubo 3D mvc

---
+++ 1 - OpenTK/Tareas/1 2025/Figura3D-MVC_S/Figura3D-MVC/Views/GameWindow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figura3D_MVC.Views
+{
+    class GameWindow
+    {
+    }
+}
