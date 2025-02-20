1d5e900d9cb0dece14a229c8ae4a3b9f9bc02a20
jhasmany-fernandez <jhasmany.fernandez.dev@gmail.com>
2025-02-20 12:39:48 -0400

    cubo 3D mvc terminado comentando and update readme

--- 1 - OpenTK/Tareas/1 2025/Figura3D-MVC_S/Figura3D-MVC/Program.cs
+++ 1 - OpenTK/Tareas/1 2025/Figura3D-MVC_S/Figura3D-MVC/Program.cs
@@ -1,32 +1,31 @@
 ﻿using System;
 using System.Threading;
 using System.Windows.Forms;
-using crearFigruas3D.Controllers;
-using crearFigruas3D.Views;
+using crearFigruas3D.Controllers; // Se importa el espacio de nombres para los controladores
+using crearFigruas3D.Views;       // Se importa el espacio de nombres para las vistas
 
 namespace crearFigruas3D
 {
     static class Program
     {
-        [STAThread]
+        [STAThread] // Atributo que indica que el hilo principal es de un solo hilo (STA) para que funcione correctamente con la interfaz gráfica.
         static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+        {   
+            Application.EnableVisualStyles(); // Habilitar estilos visuales en la aplicación para que se utilicen las opciones de apariencia modernas.
+            Application.SetCompatibleTextRenderingDefault(false); // Configura si el renderizado de texto será compatible con la forma predeterminada de los formularios.
 
-            GameController controller = new GameController();
+            GameController controller = new GameController(); // Crear una instancia del controlador que manejará la lógica de la aplicación.
 
-            // Ejecutar Windows Forms en un hilo separado
+            // Crear un hilo para ejecutar la interfaz gráfica de usuario (UI) de Windows Forms
             Thread uiThread = new Thread(() =>
             {
-                Application.Run(new MainForm(controller));
-            });
+                Application.Run(new MainForm(controller)); // Ejecutar el formulario principal de la aplicación en el hilo de la UI.
+            });                                            // El formulario 'MainForm' recibe como parámetro el controlador 'controller'.
 
-            uiThread.SetApartmentState(ApartmentState.STA); // Requerido para UI
-            uiThread.Start();
+            uiThread.SetApartmentState(ApartmentState.STA); // Establecer el estado del hilo como STA (Single-Threaded Apartment), necesario para la ejecución de la interfaz gráfica.
+            uiThread.Start(); // Iniciar el hilo de la interfaz gráfica de usuario.
 
-            // Ejecutar OpenTK en el hilo principal
-            controller.Run();
-        }
+            controller.Run(); // Ejecutar OpenTK (que podría estar manejando la lógica 3D) en el hilo principal.
+        }                     // Esto permite que la parte gráfica y la lógica del juego se ejecuten de manera separada y sin bloquear la interfaz de usuario.
     }
 }
--- README.md
+++ README.md
@@ -1,19 +1,27 @@
 # U.A.G.R.M.-Programacion_Grafica
 
-U.A.G.R.M. | Materia: Programacion Grafica | Docente: Miguel Jesus Peinado | Repositorio: Avance 1_2024 | Lenguaje: C# | IDE: Visual Studio Comunity
+U.A.G.R.M. | Materia: Programacion Grafica | Docente: Miguel Jesus Peinado | Repositorio: Avance 1_2025 | Lenguaje: C# | IDE: Visual Studio Comunity
 
 <p align="center"><img src="https://user-images.githubusercontent.com/36086876/146686931-7454e35d-a44b-422f-84c6-c3645d235ad3.png" width="162"></p>
 <p align="center"><img src="https://user-images.githubusercontent.com/36086876/148548585-d4259cff-b909-48de-8d48-c41a7ba2cab3.png" width="292"></p>
 
 ## NUMERO DE VISITANTES.
+<img align="left" src = "https://profile-counter.glitch.me/U.A.G.R.M.-Programacion_Grafica/count.svg" alt ="Loading"> <br>
 
 ---
 
-## PROYECTOS:
+## DATOS:
++ Visual Estudio: 2022
++ OpenTK: 3.3.1
++ .Net: 4.7.2
+
+---
+
+## TAREAS:
 
 > [+ Poryectos I - OPENTK](https://github.com/jhasmany-fernandez/F.I.C.C.T.-Proyecto_I-OpenTK-SA-INF102.git)
 >
-> > [Tarea # 1](https://github.com/uagrm-developer-community-sw/U.A.G.R.M.-Programacion_Grafica/tree/main/1%20-%20OpenTK/Tareas/Tarea1)
+> > [Tarea # 1](https://github.com/uagrm-developer-community-sw/U.A.G.R.M.-Programacion_Grafica/tree/main/1%20-%20OpenTK/Tareas/Tarea1)<br>
 > > [Tarea # 2](https://github.com/uagrm-developer-community-sw/U.A.G.R.M.-Programacion_Grafica/tree/main/1%20-%20OpenTK/Tareas/Tarea2_S)
 
 > [+ Poryectos II - ROBLOX](https://github.com/jhasmany-fernandez/F.I.C.C.T.-Proyecto_II-Roblox-SA-INF102.git)
@@ -22,14 +30,14 @@ U.A.G.R.M. | Materia: Programacion Grafica | Docente: Miguel Jesus Peinado | Rep
 
 ## TAREAS - ANTERIORES:
 
-> [Archivos y Documentos - 2 2023](https://github.com/uagrm-developer-community-sw/U.A.G.R.M.-Programacion_Grafica/tree/main/Tareas/Tareas%201%202023)
+> [Archivos y Documentos - 2 2023](https://github.com/uagrm-developer-community-sw/U.A.G.R.M.-Programacion_Grafica/tree/main/Tareas/Tareas%201%202023)<br>
 > [Archivos y Documentos - 1 2023]()
 
 ---
 
 ## CLASES ONLINE:
 
-> [+ Clases Grabadas - Semestre 1 2023| Youtube](https://youtube.com/playlist?list=PLZh2aEFREMprtC5kX9TxbRihWZnyn6EjU)
+> [+ Clases Grabadas - Semestre 1 2023 - 2024 | Youtube](https://youtube.com/playlist?list=PLZh2aEFREMprtC5kX9TxbRihWZnyn6EjU)
 
 ## REPOSITORIO.
 
